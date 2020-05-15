10fae3f5d992a84a7b8143b693113b2fe92dfaa0
jalal Arif <jalalharfjalal@gmail.com>
2020-05-15 03:22:56 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,7 +5,7 @@ const babel = require('babel-core');
 
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
-let companyName = 'Scuber';
+
 let mostProfitableNeighborhood='Chelsea'
 let companyCeo='Susan Smith';
 
